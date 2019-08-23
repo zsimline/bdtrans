a2ab3a5b3a5400f140aeb6be4c7b33a5d6a1a424
zsimline <zsimline@163.com>
2019-08-23 22:24:59 +0800

    0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.MD', 'r') as f:
 
 setuptools.setup(
     name = 'bdtrans',
-    version = '0.3.0',
+    version = '0.3.1',
     author = 'Mxsyx',
     author_email = 'zsimline@163.com',
     description = 'A library for china baidu translation',
